3c148bfd9353072cac033a86bf87d402c3988619
studyjyj <130561552+studyjyj@users.noreply.github.com>
2023-04-18 00:40:04 +0900

    fourth

---
+++ ch12_jdbcTemplate/src/main/java/com/lec/ex12/service/BContentService.java
@@ -0,0 +1,14 @@
+package com.lec.ex12.service;
+import java.util.Map;
+import org.springframework.ui.Model;
+
+import com.lec.ex12.dao.BoardDao;
+public class BContentService implements Service {
+	@Override
+	public void execute(Model model) {
+		Map<String, Object> map = model.asMap();
+		int bid = (Integer) map.get("bid");
+		BoardDao bDao = new BoardDao();
+		model.addAttribute("contentBoard", bDao.content(bid));
+	}
+}
---
+++ ch12_jdbcTemplate/src/main/java/com/lec/ex12/utile/Constant.java
@@ -0,0 +1,7 @@
+package com.lec.ex12.utile;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Constant {
+	public static JdbcTemplate template;
+}
